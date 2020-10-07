4d1a57d8cac48d2eaeb3efb6c362711ea6a38be9
19101990 <e19101990@gmail.com>
2020-10-07 09:49:14 +0200

    add scroll to 'Home' nav-link

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Hero() {
     return (
     <>
-      <div className="hero">
+      <div className="hero" id="topPage">
         <div className="hero_txt">
           <h1>Witaj na mojej stronie!</h1>
           <p>Jestem doświadczonym trenerem personalnym z bardzo dużą wiedzą w dziedzinie fizjoterapii zdobytą na studiach oraz dodatkowych kursach. Swoim podopiecznym oferuję profesjonalną i kompleksową opiekę.</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import './Navbar.css'
 import logo from '../img/logo-svg-transp.svg';
 
 
+
 function Navbar() {
 
     // scrollToTop = () => {
@@ -43,22 +44,22 @@ function Navbar() {
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item nav-item_active">
-                        <Link  activeClass="active" to='home' spy={true} smooth={true} offset={-81} duration={500} className='nav-links' onClick={closeMobileMenu}>
+                        <Link  activeClass="active" to='topPage' spy={true} smooth={true} offset={-80} duration={500} className='nav-links' onClick={closeMobileMenu}>
                                 Home
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link  activeClass="active" to='offer' spy={true} smooth={true} offset={-81} duration={500} className='nav-links' onClick={closeMobileMenu}>
+                            <Link  activeClass="active" to='offer' spy={true} smooth={true} offset={-80} duration={500} className='nav-links' onClick={closeMobileMenu}>
                                 Oferta
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link  activeClass="active" to='about' spy={true} smooth={true} offset={-81} duration={500} className='nav-links' onClick={closeMobileMenu}>
+                            <Link  activeClass="active" to='about' spy={true} smooth={true} offset={-80} duration={500} className='nav-links' onClick={closeMobileMenu}>
                                 O mnie
                             </Link>
                         </li>
                         <li className="nav-item">
-                        <Link  activeClass="active" to='contact' spy={true} smooth={true} offset={-81} duration=  {500} className='nav-links' onClick={closeMobileMenu}>
+                        <Link  activeClass="active" to='contact' spy={true} smooth={true} offset={-80} duration=  {500} className='nav-links' onClick={closeMobileMenu}>
                                 Kontakt
                             </Link>
                         </li>
