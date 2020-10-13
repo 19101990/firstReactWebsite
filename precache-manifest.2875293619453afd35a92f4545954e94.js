78d958087b513ac74e4de8300023f89dedbb03bb
Ewa Hostyńska <e19101990@gmail.com>
2020-10-13 15:56:43 +0200

    Updates

---
+++ precache-manifest.2875293619453afd35a92f4545954e94.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bcb4b85a8c3c58bbf20c5830fe23e7",
+    "url": "/fizjotrener/index.html"
+  },
+  {
+    "revision": "c9cb215f16e5e64b0719",
+    "url": "/fizjotrener/static/css/main.4cc1f2b6.chunk.css"
+  },
+  {
+    "revision": "01641ce38ae5627cd97b",
+    "url": "/fizjotrener/static/js/2.87ee9db1.chunk.js"
+  },
+  {
+    "revision": "a45201720a0efd981f039d141fc0c42a",
+    "url": "/fizjotrener/static/js/2.87ee9db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9cb215f16e5e64b0719",
+    "url": "/fizjotrener/static/js/main.b69234c7.chunk.js"
+  },
+  {
+    "revision": "62b45a36c9b2666f6c3a",
+    "url": "/fizjotrener/static/js/runtime-main.a8443a63.js"
+  },
+  {
+    "revision": "f41a11820b4297cfd58a21a0e70c0fb1",
+    "url": "/fizjotrener/static/media/about_img.f41a1182.jpg"
+  },
+  {
+    "revision": "5c534a9442e120ec9d6fa3727d2434a5",
+    "url": "/fizjotrener/static/media/gym_img.5c534a94.jpg"
+  },
+  {
+    "revision": "6b3516d8a1e34e8fefdfa9407871bc48",
+    "url": "/fizjotrener/static/media/hero_img.6b3516d8.jpg"
+  },
+  {
+    "revision": "4245eb208c86ee99b8d052714aabddb6",
+    "url": "/fizjotrener/static/media/home_img.4245eb20.jpg"
+  },
+  {
+    "revision": "0c4765726bbb637c1b82171fe5dae434",
+    "url": "/fizjotrener/static/media/logo-svg-transp.0c476572.svg"
+  },
+  {
+    "revision": "17c842bdbe75985184c4ea8515995a4c",
+    "url": "/fizjotrener/static/media/office_img.17c842bd.jpg"
+  },
+  {
+    "revision": "ec30cd802b20bdb04e7c0a6c571246dc",
+    "url": "/fizjotrener/static/media/outdoor_img.ec30cd80.jpg"
+  },
+  {
+    "revision": "ff07a2b17702705ff9e5440c5bf1b01c",
+    "url": "/fizjotrener/static/media/pair_img.ff07a2b1.jpg"
+  }
+]);
